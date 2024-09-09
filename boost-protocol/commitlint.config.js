a226c8501ef523d23b31b8d3ca2c15a5353a0584
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-09-09 17:15:10 +0200

    Delete scope directories

--- boost-protocol/commitlint.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'subject-case': [0, 'always', ['start-case', 'pascal-case', 'upper-case']],
-  },
-};
--- boost-protocol/packages/sdk/hardhat.config.cjs
+++
@@ -1,5 +0,0 @@
-require('@nomicfoundation/hardhat-toolbox-viem');
-
-module.exports = {
-  solidity: '0.8.24',
-};
